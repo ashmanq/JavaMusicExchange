3a8d57fcddacb53860f7143d5c0a0a604e82099a
Ashir <ashirq@outlook.com>
2020-06-05 14:33:51 +0100

    Created Shop class and tested it.

---
+++ src/main/java/Shop.java
@@ -0,0 +1,30 @@
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
+public class Shop {
+
+    ArrayList<ISell> shopItems;
+    String name;
+
+    public Shop(String name) {
+        this.name = name;
+        this.shopItems = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addItem(ISell item) {
+        this.shopItems.add(item);
+    }
+
+    public  void removeItem(ISell item) {
+        this.shopItems.remove(item);
+    }
+
+    public int noOfItems() {
+        return this.shopItems.size();
+    }
+}
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,46 @@
+import behaviours.ISell;
+import items.GrandPianoItem;
+import items.LargeDrumSetItem;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopTest {
+
+    Shop shop;
+    ISell instrument1, instrument2;
+
+    @Before
+    public void before() {
+        instrument1 = new LargeDrumSetItem("A large drum set.", 200, 300);
+        instrument2 = new GrandPianoItem("A grand piano.", 10000, 13000);
+        shop = new Shop("Ash's Musical Emporium");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Ash's Musical Emporium", shop.getName());
+    }
+
+    @Test
+    public void startsWithNoItems() {
+        assertEquals(0, shop.noOfItems());
+    }
+
+    @Test
+    public void canAddItem() {
+        shop.addItem(instrument1);
+        assertEquals(1, shop.noOfItems());
+    }
+
+    @Test
+    public void canRemoveItem() {
+        shop.addItem(instrument1);
+        shop.addItem(instrument2);
+        assertEquals(2, shop.noOfItems());
+        shop.removeItem(instrument1);
+        assertEquals(1, shop.noOfItems());
+    }
+}
